31647d2a1c587c661f2e524fd91edce48720aacb
BRIAN BETT <+>
2023-05-20 07:46:38 +0300

    completed"

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+    attr_writer :title
+    attr_accessor :page_count, :genre, :author
+    attr_accessor :title
+
+    def initialize(title = "And Then There Were None")
+        @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
 
